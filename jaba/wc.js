80e3b501569bb1c4b6831bf06d6db48cc0d39bb5
Jaba Giqoshvili <jabagiqoshvili@yahoo.com>
2014-01-26 17:25:45 +0400

    davamate byte - ebis datvlis funqcia

--- jaba/wc.js
+++ jaba/wc.js
@@ -1,18 +1,19 @@
 var fs = require('fs');
-fs.readFile('1.txt', 'utf8', function (err, data) {
+fs.readFile('test.txt', 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   }
-  console.log('number of words is',cword(data));
-  console.log('number of lines is',cline(data));
-  console.log('number of lines is',(data.length));
+  console.log('number of words is' ,cword(data));
+  console.log('number of lines is' ,cline(data));
+  console.log('number of lines is' ,(data.length));
+  console.log('number of bytes is' ,bytes(data));
 });
 
 function cword(w) {
   var count = 0,
     words = w.split(/\s/g);
 
-  console.log(words);
+  //console.log(words);
 
   for (i = 0; i < words.length; i++) {
     if (words[i] != '') {
@@ -35,3 +36,24 @@ function cline(l) {
   }
   return countl;
 }
+
+function bytes(b) {
+  var countB = 0;
+  for (var n = 0; n < b.length; n++) {
+    var c = b.charCodeAt(n);
+    if (c < 128) {
+      countB++;
+    }
+    else if((c > 127) && (c < 2048)) {
+      countB = countB+2;
+    }
+    else {
+      countB = countB+3;
+    }
+  }
+  return countB;
+}
+
+
+
+
