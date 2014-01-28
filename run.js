528126dde22e7a7f548a023e93bfa7f83693652d
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-01-29 02:16:34 +0400

    Add readDir to run.js

--- run.js
+++ run.js
@@ -1,1 +1,13 @@
+var fs = require('fs');
 
+var name = 'wc',
+  testEnvs  = {
+    jaba : 'node wc.js',
+    //jani : './wc',
+    //nodo : './wc'
+  },
+  testFiles = fs.readdirSync('tests');
+
+for(var i = 0; i < testFiles.length; i++) {
+  console.log(testFiles[i]);
+}
