3049d72fd654184f9011000929efdeb537a81d79
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-01-30 16:47:09 +0400

    Add async apply of files

--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 var fs = require('fs'),
     exec = require('child_process').exec,
     args = require('commander'),
+    async = require('async'),
     package = require('./package.json');
 
 args
@@ -11,6 +12,7 @@ args
   .option('-c', 'print characters count')
   .option('--program <string>', 'select concrete program to test')
   .option('--list', 'show list of programs')
+  //.option('--level <string>', 'level of test (more deeper and harder)')
   .parse(process.argv);
 
 var name = 'wc',
@@ -20,7 +22,8 @@ var name = 'wc',
     //nodo : './wc'
   },
   env = null
-  testFiles = fs.readdirSync('tests');
+  testDir   = './tests',
+  testFiles = fs.readdirSync(testDir);
 
 if(args.list) {
   console.log('  Avaliable programs:');
@@ -34,4 +37,22 @@ if (args.program && (env = testEnvs[args.program]) == null) {
   console.log('\n  Program "' + args.program + '" not found');
   args.help();
 }
+
+var wcResults   = {},
+    wcCallbacks = [],
+    results     = {},
+    callbacks   = {};
+
+for(var i = 0; i < testFiles.length; i++) {
+  wcCallbacks[i] = async.apply(function(file, callback) {
+    exec('wc ' + testDir + '/' + file, function(error, stdout, stderr) {
+      callback(error, stdout);
+    });
+  }, testFiles[i]);
+}
+
+async.parallel(wcCallbacks, function(err, results) {
+  wcResults = results;
+  console.log(wcResults);
+});
 
