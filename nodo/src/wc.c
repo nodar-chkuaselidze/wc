db7dccfbe5379c855c8b966ee81bc97588882764
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-02-26 00:51:37 +0400

    implement minimal getopts and flags

--- nodo/src/wc.c
+++ nodo/src/wc.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
 
-int main() {
-  printf("%d", 123);
+int main (int argc, char **argv) {
+  unsigned short int flags = 0;
+  char c;
+
+  while ((c = getopt(argc, argv, "clmw")) != -1)
+  {
+    switch (c) {
+      //number of multi-bytes
+      case 'm': //0x01
+        flags |= 0x01;
+        flags &= ~0x02;
+        break;
+
+      //number of bytes
+      case 'c': //0x02
+        flags |= 0x02;
+        flags &= ~0x01;
+        break;
+
+      //number of lines
+      case 'l': //0x04
+        flags |= 0x04;
+        break;
+
+      //number of words
+      case 'w': //0x08
+        flags |= 0x08;
+        break;
+
+      default:
+        abort();
+    }
+  }
   return 0;
 }
