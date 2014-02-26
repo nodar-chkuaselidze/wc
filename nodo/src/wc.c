16c2ca61a6c6da6b7a26d02acabf6bc438ac246d
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-02-27 01:51:07 +0400

    reorder comments

--- nodo/src/wc.c
+++ nodo/src/wc.c
@@ -17,24 +17,24 @@ int main (int argc, char **argv) {
   while ((c = getopt(argc, argv, "clmw")) != -1)
   {
     switch (c) {
-      //number of multi-bytes
-      case 'm': //0x01
+      //number of multi-bytes - 0x01
+      case 'm':
         flags |= 0x01;
         flags &= ~0x02;
         break;
 
-      //number of bytes
-      case 'c': //0x02
+      //number of bytes - 0x02
+      case 'c':
         flags |= 0x02;
         flags &= ~0x01;
         break;
 
-      //number of lines
-      case 'l': //0x04
+      //number of lines - 0x04
+      case 'l':
         flags |= 0x04;
         break;
 
-      //number of words
+      //number of words - 0x08
       case 'w': //0x08
         flags |= 0x08;
         break;
